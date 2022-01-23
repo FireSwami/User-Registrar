8773fa61a206f68eee70b5c801801c1e55115c5b
FireSwami <jobforsoul@gmail.com>
2022-01-23 17:49:35 +0200

    docker build

--- hello_site/settings.py
+++ hello_site/settings.py
@@ -125,6 +125,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+
+CORS_ORIGIN_WHITELIST =[
+
+    "http://localhost:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8000",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
